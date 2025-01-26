232feafe31c304b9cbfc5eec81274ef093bc6b12
SoufianeMoussaoui <dgiugfha098@gmail.com>
2025-01-26 14:03:56 +0100

    Update GenerateClass.cpp

--- App/src/GenerateClass.cpp
+++ App/src/GenerateClass.cpp
@@ -9,11 +9,12 @@
 #include <string>
 #include <windows.h>
 #include <shlobj.h> 
+#include <typeinfo>
 
 using namespace std;
 
 
-GenerateClass::GenerateClass(const string& className): className(className){}
+GenerateClass::GenerateClass(const string& className, bool port = false): className(className){}
 
 void GenerateClass::setClassName(string Name){className = Name;}
 string GenerateClass::getClassName() const{return className;}
@@ -22,6 +23,7 @@ string GenerateClass::getFileName(){return className + ".h";}
 
 
 void GenerateClass::createFile(){
+
 	string filename = getFileName();
 	ofstream headerFile(filename);
 	if(!headerFile.is_open()){
@@ -29,15 +31,47 @@ void GenerateClass::createFile(){
         return;
 	}
 	// The header of the file:
-	  headerFile << "#pragma once" << endl << "#include <iostream>" << endl <<"#include <string>" << endl;
+	headerFile << "#pragma once" << endl << "#include <iostream>" << endl <<"#include <string>" << endl;
+	headerFile << "using namespace std;" << endl;
     headerFile << "\rclass " << className << "{" << endl;
     // check if there is anay attribute
-  
-    	headerFile << "\rpublic : " << endl << "\r\t" << constructorByDefault(className) << endl;
-    	headerFile << "\t" << constructorByCopie(className) << endl;
-    	headerFile << "\t" << destructor(className) << endl;
-    	headerFile << "};" << endl;
+    
+    /*if(attributeExiste()){
+    	if(!port){
+    		// private
+    	}
+    	
+    }
+    */
+    headerFile << "\rpublic : " << endl << "\r\t" << constructorByDefault(className) << endl;
+    headerFile << "\t" << constructorByCopie(className) << endl;
+    headerFile << "\t" << destructor(className) << endl;
+    if(attributeExiste()){
+    	headerFile << "\t" << constructorByArgument(className);
+    	for (auto it = attributesListe.begin(); it != attributesListe.end(); ++it){
+    		auto test = it;
+    		if (++test == attributesListe.end())
+    			headerFile << getVarType(it->second);
+    		else
+    			headerFile << getVarType(it->second) << ", ";
+    	}
+    	headerFile << ");" << endl;
 
+    	vector<string> getters;
+    	vector<string> setters;
+    	createGetter(getters);
+    	createSetter(setters);
+
+    	headerFile << "\t\r//Getters : " << endl;
+    	for(int i = 0; i<getters.size(); i++)
+    		headerFile << "\t" << getters[i] << endl;
+ 
+    	headerFile << "\t\r//Setters: " << endl;
+    	for(int i = 0; i<setters.size(); i++)
+    		headerFile << "\t" << setters[i] << endl;
+
+    }
+    headerFile << "\r};" << endl;
 }
 
 
@@ -52,11 +86,12 @@ void GenerateClass::clearFile(){
 }
 
 
-//bool GenerateClass::attributeExiste(){return attributesListe.size() > 0;}
+bool GenerateClass::attributeExiste(){return !(attributesListe.empty());}
 
 string constructorByDefault(string& name){return name + "();";}
 string constructorByCopie(string& name){return name + "(" + name + "&);";}
 string destructor(string& name){return "~" + name + "();";}
+string constructorByArgument(string& name){return name + "(";}
 
 string getDesktopPath() {
     char path[MAX_PATH];
@@ -66,10 +101,30 @@ string getDesktopPath() {
     return "";
 }
 
+string getVarType(const any& value){
+	if (value.type() == typeid(int))
+        return "int";
+    else if (value.type() == typeid(string)) 
+        return "string";
+    else 
+        return "double";
+    
+}
 
 
 
-
+void GenerateClass::createGetter(vector<string>& getters){
+	for(auto it = attributesListe.begin(); it != attributesListe.end(); ++it){
+		string getFunction = getVarType(it->second) + " get" + it->first + "() const;";
+		getters.push_back(getFunction);
+	}
+}
+void GenerateClass::createSetter(vector<string>& setters){
+	for(auto it = attributesListe.begin(); it != attributesListe.end(); ++it){
+		string setFunction = "void set" + it->first + "(" + getVarType(it->second) + ");" ;
+		setters.push_back(setFunction);
+	}
+}
 
 
 	
